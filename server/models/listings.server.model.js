2495aa4502b53fd072dcfb61f87113ce8a420a96
dmeadows <meadows.david@siemens.com>
2019-09-23 11:48:18 -0400

    made changes to express.js
    copied Schema code from bootcamp#2 and placed it in listings.server.model.js

--- server/models/listings.server.model.js
+++ server/models/listings.server.model.js
@@ -1,20 +1,34 @@
 //You can replace this entire file with your Bootcamp Assignment #2 - ListingSchema.js File
 
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here from Bootcamp Assignment #2 - ListingSchema.js File*/
-    
+    code: {type: String, required: true},
+    name: {type: String, required: true},
+    coordinates: {
+        latitude: {type: Number, required: false},
+        longitude: {type: Number, required: false}
+    },
+    address: {type: String, required: false}
 });
 
 /* create a 'pre' function that adds the updated_at and created_at if not already there property */
-listingSchema.pre('save', function(next) {
-  /* your code here from Bootcamp Assignment #2 - ListingSchema.js File */
+listingSchema.pre('save', function (next) {
+    // get the current date
 
-  next();
+    var currentDate = new Date();
+    // change the updated_at field to current date
+    this.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+
+    next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
